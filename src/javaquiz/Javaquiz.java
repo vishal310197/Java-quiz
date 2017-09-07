23747b3391e7a626a24e243ff379fb79cf416fdd
vishal310197 <31555109+vishal310197@users.noreply.github.com>
2017-09-07 09:11:23 +0530

    Add files via upload

---
+++ src/javaquiz/Javaquiz.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaquiz;
+
+/**
+ *
+ * @author Ideapad
+ */
+public class Javaquiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new startform().setVisible(true);
+        // TODO code application logic here
+    }
+}
